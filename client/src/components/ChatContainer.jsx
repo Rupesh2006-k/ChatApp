b4f79c055eb4c0bd4daaa2c8734475cd2945c1bb
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-01-13 15:49:05 +0530

    chat container is done

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import assets, { messagesDummyData } from "../assets/images/assets";
+import { formatMessageTime } from "../libs/utils";
 
 const ChatContainer = ({ setSelectedUser, selectedUser }) => {
-  let scrollEnd = useRef(null)
+  let scrollEnd = useRef(null);
+
   useEffect(() => {
-   if(scrollEnd.current ){
-    scrollEnd.current.scrollIntoView({behavior:"smooth"}) 
-   }
-  }, [])
-  
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return selectedUser ? (
-    <div className="h-full overflow-scroll relative backdrop-blur-lg">
+    <div
+      className="h-full overflow-y-scroll relative backdrop-blur-lg
+      [&::-webkit-scrollbar]:hidden
+      [-ms-overflow-style:none]
+      [scrollbar-width:none]"
+    >
       {/* ------header--------- */}
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
@@ -29,36 +36,83 @@ const ChatContainer = ({ setSelectedUser, selectedUser }) => {
 
         <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
       </div>
+
       {/* --------chat area -------- */}
-      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+      <div
+        className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6
+        [&::-webkit-scrollbar]:hidden
+        [-ms-overflow-style:none]
+        [scrollbar-width:none]"
+      >
         {messagesDummyData.map((msg, index) => (
           <div
-            key={index} className={`flex items-end gap-2 justify-end ${ msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse justify-start" }`}>
+            key={index}
+            className={`flex items-end gap-2 justify-between ${
+              msg.senderId !== "680f50e4f10f3cd28382ecf9" &&
+              "flex-row-reverse justify-start"
+            }`}
+          >
             {msg.image ? (
-              <img src={msg.image} className="max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8"/>
+              <img
+                src={msg.image}
+                className="max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8"
+                alt=""
+              />
             ) : (
-              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${ msg.senderId === "680f50e4f10f3cd28382ecf9" ? "rounded-br-none" : "rounded-bl-none"}`}>
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}
+              >
                 {msg.text}
               </p>
             )}
+
             <div className="text-center text-xs">
-             <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin}
-              alt="" className="w-7 rounded-full" />
-              <p className="text-gray-500">{msg.createdAt}</p>
+              <img
+                src={
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? assets.avatar_icon
+                    : assets.profile_martin
+                }
+                alt=""
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500">
+                {formatMessageTime(msg.createdAt)}
+              </p>
             </div>
           </div>
         ))}
 
         <div ref={scrollEnd}></div>
       </div>
+
+      {/* ------------bottom area--------------------- */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex flex-1 items-center bg-gray-100/12 px-3 rounded-full">
+          <input
+            type="text"
+            placeholder="Send a message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
+          />
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              className="w-5 mr-2 cursor-pointer"
+              alt=""
+            />
+          </label>
+        </div>
+        <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
+      </div>
     </div>
   ) : (
-    <div
-      className="fIex flex-col items-center justify-center gap-2 text-gray-500
-bg-white/10 max-md:hidden"
-    >
+    <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
       <img src={assets.logo_icon} className="max-w-16" alt="" />
-
       <p className="text-lg font-medium text-white">Chat anytime . anywhere</p>
     </div>
   );
