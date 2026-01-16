f06654eb20ea74d366a6768659755e6d2eed22ca
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-01-16 16:44:25 +0530

    backend updated

--- server/server.js
+++ server/server.js
@@ -82,10 +82,12 @@ app.use("/api/messages", messageRoute);
 
 // ---------------- SERVER START ----------------
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
+// if (process.env.NODE_ENV !== "production") {
+  
+// }
+
+const PORT = process.env.PORT || 3000;
 
   server.listen(PORT, () => {
     console.log("Server is running on port:", PORT);
   });
-}
